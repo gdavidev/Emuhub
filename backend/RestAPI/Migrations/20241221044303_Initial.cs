d525bb68edddbb65dd85e0823405c9050c749049
David <77506410+gdavidev@users.noreply.github.com>
2025-01-27 11:32:55 -0300

    feat: Base game controller and models with DTOs and database configuration

---
+++ backend/RestAPI/Migrations/20241221044303_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
--- backend/RestAPI/Program.cs
+++ backend/RestAPI/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+	opt.UseSqlServer(
+		builder.Configuration.GetConnectionString("DefaultConnection"),
+		options => options.EnableRetryOnFailure(
+			maxRetryCount: 5,
+			maxRetryDelay: System.TimeSpan.FromSeconds(30),
+			errorNumbersToAdd: null
+		)
+	)
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,10 +29,11 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+// Middlewares
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+// Endpoint mapping
 app.MapControllers();
 
 app.Run();
