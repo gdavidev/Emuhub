1ec0ac86841e84ffa3b6c15a798f1727a4a2a794
David <77506410+gdavidev@users.noreply.github.com>
2025-03-15 00:32:59 -0300

    fix: Initial migrations re-added and duplicated tables error solved on "dotnet ef database update"

--- backend/Emuhub.Infrastructure/DataAccess/ApplicationDbContext.cs
+++ backend/Emuhub.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -14,7 +14,6 @@ public class ApplicationDbContext : DbContext
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
