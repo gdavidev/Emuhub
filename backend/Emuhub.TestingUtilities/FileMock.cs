5a1bf56006733d1c57867283542e395846f94af3
David <77506410+gdavidev@users.noreply.github.com>
2025-03-19 09:48:52 -0300

    test: Created File creator testing utility

---
+++ backend/Emuhub.TestingUtilities/FileMock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.Internal;
+
+namespace Emuhub.TestingUtilities
+{
+	public class FileMock
+	{
+		public static FormFile Create(string filename)
+		{
+			return new FormFile(new MemoryStream([]), 0, 0, "", filename);
+		}
+
+		public static FormFile Create(string filename, Stream content)
+		{
+			return new FormFile(content, 0, content.Length, "", filename);
+		}
+	}
+}
