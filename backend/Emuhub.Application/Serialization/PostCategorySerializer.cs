332f84561c16e62e262eac08e3ea14cb26713088
David <77506410+gdavidev@users.noreply.github.com>
2025-04-25 18:08:31 -0300

    feat: Added post categories listing endpoint

---
+++ backend/Emuhub.Application/Serialization/PostCategorySerializer.cs
@@ -0,0 +1,15 @@
+﻿using Emuhub.Communication.Data.PostCategories;
+using Emuhub.Domain.Entities.Forum;
+
+namespace Emuhub.Application.Serialization
+{
+    public class PostCategorySerializer
+    {
+        public static PostCategoryResponse ToPostCategoryResponse(PostCategory category) =>
+            new PostCategoryResponse()
+            {
+                Id = category.Id,
+                Name = category.Name,
+            };
+    }
+}
