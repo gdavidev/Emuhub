6a60c6ff9b4b30758dfad59c9b6675c57bc64ace
David <77506410+gdavidev@users.noreply.github.com>
2025-05-01 21:20:41 -0300

    feat: Updated Post entity and migrations added

--- backend/Emuhub.Domain/Entities/Forum/Post.cs
+++ backend/Emuhub.Domain/Entities/Forum/Post.cs
@@ -7,11 +7,13 @@ public class Post
     public long Id { get; set; }
     public required string Title { get; set; }
     public required string Content { get; set; }
-    public required User Author { get; set; }
-    public required PostCategory Category { get; set; }
-    public required bool IsActive { get; set; }
-    public string? Image { get; set; }
-    public required List<Comment> Comments { get; set; }
     public required DateTime CreateAt { get; set; }
     public required DateTime UpdatedOn { get; set; }
+    public required bool IsActive { get; set; }
+
+    public required string UserId { get; set; }
+
+    public List<Comment> Comments { get; set; } = [];
+	public User? Author { get; set; }
+    public PostCategory? Category { get; set; }
 }
